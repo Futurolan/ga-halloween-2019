f3ae7f55eb3a2b7a7d5e2601bdd12153e1e8f4d2
bmagic <baptiste@bmagic.fr>
2019-09-13 11:02:02 +0200

    Update Date & Ga Code

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
 let config = {
   title: 'Gamers Assembly : Halloween Edition 2018',
   logo: `/static/img/logo-orange-100x100.png`,
-  description: 'L\'édition 2018 de la Gamers Assembly Halloween Edition aura lieu du ?? au ?? novembre à la salle de spectacle de la Hune -Saint - Benoît(86)',
+  description: 'L\'édition 2018 de la Gamers Assembly Halloween Edition aura lieu du 08 au 11 novembre à la salle de spectacle de la Hune -Saint - Benoît(86)',
   metaImagePath: `/static/img/logo-orange-200x200.png`,
-  gaTrackingId: 'UA-128777022-2',
+  gaTrackingId: 'UA-128777022-7',
   mainPartner: {
     logo: '/static/img/logo_st_benoit.jpg',
     url: 'http://www.ville-saint-benoit.fr/'
