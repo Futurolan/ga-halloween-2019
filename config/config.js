c713a2e9267329bfba9074e9fd068ad4a267dcae
bmagic <baptiste@bmagic.fr>
2019-09-13 11:05:07 +0200

    Fix title on GA HE 2019

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 let config = {
-  title: 'Gamers Assembly : Halloween Edition 2018',
+  title: 'Gamers Assembly : Halloween Edition 2019',
   logo: `/static/img/logo-orange-100x100.png`,
   description: 'L\'édition 2018 de la Gamers Assembly Halloween Edition aura lieu du 08 au 11 novembre à la salle de spectacle de la Hune -Saint - Benoît(86)',
   metaImagePath: `/static/img/logo-orange-200x200.png`,
